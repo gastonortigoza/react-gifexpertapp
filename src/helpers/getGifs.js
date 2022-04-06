f0edb441a3148e5898fc8fe8c2e35b5cebcb2ed6
Guido Gaston Ortigoza <gastonguidoortigoza@gmail.com>
2022-04-05 23:04:21 -0300

    usando https

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-    const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=1NSvEIRIYrhoTgvPjIGVtnk6HduVoLbP`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=1NSvEIRIYrhoTgvPjIGVtnk6HduVoLbP`;
     const resp = await fetch(url);
     const { data } = await resp.json();
 
